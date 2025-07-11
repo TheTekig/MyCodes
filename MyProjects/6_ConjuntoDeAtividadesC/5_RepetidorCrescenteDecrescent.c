24290451c3439e0551d53acdd8a222148d099049
Diogo Teodoro <diogo.teodoro015@gmail.com>
2025-07-11 09:09:58 -0300

    Create 5_RepetidorCrescenteDecrescent.c
    Programa onde você entrega o número inicial e o final e gera ordem numérica até aquele número detectando se a ordem é crescente ou decrescente.

---
+++ MyProjects/6_ConjuntoDeAtividadesC/5_RepetidorCrescenteDecrescent.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+int valinicial, valfinal;
+    
+    printf("\n\t-=-Programa que imprime os numeros de um intervalo-=-\n");
+    
+    printf("\nDigite o valor inicial: ");
+    scanf("%d", &valinicial);
+    
+    printf("Digite o valor final: ");
+    scanf("%d", &valfinal);
+
+    if (valinicial < valfinal)
+    {
+        printf("\nOrdem crescente:\n\n");
+    while (valinicial <= valfinal)
+        {
+            printf("%d\n", valinicial);
+            valinicial++;
+        }
+    }
+    else
+    {
+        printf("Ordem decrescente:\n");
+        while (valinicial >= valfinal)
+            {
+            printf("%d\n", valinicial);
+            valinicial--;
+            }
+    }
+    printf("\nFim da Lista\n");
+    
+    return 0;
+}
