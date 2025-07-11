ab86cc111905704a226cdfc3b2cabaccb167086b
Diogo Teodoro <diogo.teodoro015@gmail.com>
2025-07-11 09:47:25 -0300

    Update 5_RepetidorCrescenteDecrescent.c
    -=-Update-=-
    * Modularização com funções;
    * Opções de Impressão;
    * Impressão Num/Pare e Impares;

--- MyProjects/6_ConjuntoDeAtividadesC/5_RepetidorCrescenteDecrescent.c
+++ MyProjects/6_ConjuntoDeAtividadesC/5_RepetidorCrescenteDecrescent.c
@@ -1,9 +1,85 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+void imprimeIntervaloDecrescente(int valinicial, int valfinal, int opcao)
+{
+        while (valinicial >= valfinal)
+            {
+                if (opcao == 1)
+                {
+                    if (valinicial % 2 == 0)
+                    {
+                        printf("%d\n", valinicial);
+                    }
+                }
+                else if (opcao == 2)
+                {
+                    if (valinicial % 2 != 0)
+                    {
+                        printf("%d\n", valinicial);
+                    }
+                }
+                else
+                {
+                    printf("%d\n", valinicial);
+                }
+                
+                valinicial--;
+            }
+}
+
+void imprimeIntervaloCrescente(int valinicial, int valfinal, int opcao)
+{
+
+        while (valinicial <= valfinal)
+            {    
+                if (opcao == 1)
+                {
+                    if (valinicial % 2 == 0)
+                    {
+                        printf("%d\n", valinicial);
+                    }
+                }
+                else if (opcao == 2)
+                {
+                    if (valinicial % 2 != 0)
+                    {
+                        printf("%d\n", valinicial);
+                    }
+                }
+                else
+                {
+                    printf("%d\n", valinicial);
+                }
+                
+                valinicial++;
+            }
+}
+
+int opcoes()
+{
+    int opcao;
+    printf("\n\tDeseja Imprimir:\n");
+    printf("\n1 - Sómente os pares");
+    printf("\n2 - Somente os impares");
+    printf("\n3 - Todos os numeros");
+    printf("\n\n\tOpcao: ");
+    scanf("%d", &opcao);
+    
+    while (opcao < 1 || opcao > 3)
+        {
+            printf("Opcao invalida, digite novamente: ");
+            scanf("%d", &opcao);
+        }
+    return opcao;
+}
+
+
 int main(){
     
-int valinicial, valfinal;
+int valinicial, valfinal, opcao;
     
     printf("\n\t-=-Programa que imprime os numeros de um intervalo-=-\n");
     
@@ -12,31 +88,26 @@ int valinicial, valfinal;
     
     printf("Digite o valor final: ");
     scanf("%d", &valfinal);
-
+    
+    opcao = opcoes();
+  
     if (valinicial == valfinal)
     {
         printf("\nOs valores sao iguais!\n");
     }
-    
     else if (valinicial < valfinal)
     {
         printf("\nOrdem crescente:\n\n");
-    while (valinicial <= valfinal)
-        {
-            printf("%d\n", valinicial);
-            valinicial++;
-        }
+        imprimeIntervaloCrescente(valinicial, valfinal,opcao);
     }
     else 
     {
         printf("Ordem decrescente:\n");
-        while (valinicial >= valfinal)
-            {
-            printf("%d\n", valinicial);
-            valinicial--;
-            }
+        imprimeIntervaloDecrescente(valinicial, valfinal,opcao);
     }
     printf("\nFim da Lista\n");
-    
+        
+
+        
     return 0;
 }
